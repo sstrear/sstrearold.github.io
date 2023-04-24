1265a87b3fc3cd27652245aa2baed633fbb8a7ed
sstrear <sast6985@colorado.edu>
2023-04-23 20:58:52 -0600

    wa13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -1,6 +1,6 @@
 const volumeDisplay = document.querySelector('.volume-value');
-const volumeIncreaseButton = document.querySelector('.volume-increase');
-const volumeDecreaseButton = document.querySelector('.volume-decrease');
+//const volumeIncreaseButton = document.querySelector('.volume-increase');
+//const volumeDecreaseButton = document.querySelector('.volume-decrease');
 
 let currentVolume = Math.floor(Math.random() * 101); // generate random number between 0 and 100
 volumeDisplay.textContent = currentVolume; // set initial value of volume display to random number
