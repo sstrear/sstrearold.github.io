c521d1aa692c3f91f0cdb3265233cfe37fa0da83
sstrear <sast6985@colorado.edu>
2023-04-23 20:47:25 -0600

    wa13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -1,74 +1,14 @@
+const volumeDisplay = document.querySelector('.volume-value');
 
+let currentVolume = Math.floor(Math.random() * 101); // generate random number between 0 and 100
+volumeDisplay.textContent = currentVolume; // set initial value of volume display to random number
 
-function check() {
-    console.log('test');
-}
+volumeIncreaseButton.addEventListener('click', () => {
+  currentVolume++;
+  volumeDisplay.textContent = currentVolume;
+});
 
-function submit() {
-    alert(output.textContent);
-}
-
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 100) {
-    outputInt +=1;
-    output.textContent = outputInt;
-    }
-}
-
-function random() {
-    outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
-}
-
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-  }
-
-
-
-const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
-
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
+volumeDecreaseButton.addEventListener('click', () => {
+  currentVolume--;
+  volumeDisplay.textContent = currentVolume;
+});
